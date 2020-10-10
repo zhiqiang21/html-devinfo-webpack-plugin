2fd0f1c094514340ddd0b1ddf73d2e941d9ca62b
hpuhouzhiqiang <hpuhouzhiqiang@didiglobal.com>
2020-10-10 17:42:08 +0800

    feat:兼容html-webpack-plugin@2.x

--- index.js
+++ index.js
@@ -15,49 +15,54 @@ class HtmlDevInfoWebpackPlugin {
       compiler.hooks.compilation.tap(
         'html-devinfo-webpack-plugin',
         compilation => {
-          this.compilationCallBack(compilation);
+          const htmlWebpackPluHooks = HtmlWebpackPlugin.getHooks(compilation);
+          const htmlPlugins = compilation.options.plugins.filter(
+            plugin => plugin instanceof HtmlWebpackPlugin
+          );
+
+          if (htmlPlugins.length === 0) {
+            const message =
+              "Error running html-devinfo-webpack-plugin, are you sure you have html-webpack-plugin before it in your webpack config's plugins?";
+            throw new Error(message);
+          }
+
+          htmlWebpackPluHooks.alterAssetTags.tapAsync(
+            'html-devinfo-webpack-plugin',
+            async (data, cb) => {
+              const metaData = data.assetTags.meta;
+
+              metaData.push(
+                this.metaTagString('devinfo', await this.devInfoString())
+              );
+              lodash.set(data, 'assetTags.meta', metaData);
+
+              cb(null, data);
+            }
+          );
         }
       );
     } else {
       // webpack v2
-      compiler.plugin('compilation', this.compilationCallBack(compiler));
+      compiler.plugin('compilation', (compilation) => {
+        compilation.plugin('html-webpack-plugin-alter-asset-tags', function (htmlPluginData, callback) {
+          const htmlHeadData = htmlPluginData.head || [];
+
+          htmlHeadData.push(this.metaTagString('devinfo', await this.devInfoString()));
+          lodash.set(htmlPluginData, 'head', htmlHeadData);
+
+          callback(null, htmlPluginData);
+        });
+      });
     }
   }
 
-  compilationCallBack(compilation) {
-    const htmlWebpackPluHooks = HtmlWebpackPlugin.getHooks(compilation);
-    const htmlPlugins = compilation.options.plugins.filter(
-      plugin => plugin instanceof HtmlWebpackPlugin
-    );
-
-    if (htmlPlugins.length === 0) {
-      const message =
-        "Error running html-devinfo-webpack-plugin, are you sure you have html-webpack-plugin before it in your webpack config's plugins?";
-      throw new Error(message);
-    }
-
-    htmlWebpackPluHooks.alterAssetTags.tapAsync(
-      'html-devinfo-webpack-plugin',
-      async (data, cb) => {
-        const metaData = data.assetTags.meta;
-
-        metaData.push(
-          this.metaTagString('devinfo', await this.devInfoString())
-        );
-        lodash.set(data, 'assetTags.meta', metaData);
-
-        cb(null, data);
-      }
-    );
-  }
-
-  metaTagString(name, content) {
+  metaTagString(_name, _content) {
     return {
       tagName: 'meta',
       voidTag: true,
       attributes: {
-        name,
-        content
+        _name,
+        _content
       }
     };
   }
